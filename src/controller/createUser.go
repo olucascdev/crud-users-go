2d9268ec894af68f98292fd45d70fb9a526eb0bf
olucascdev <lucasdevproject@gmail.com>
2025-09-19 15:01:04 -0300

    :safety_vest: Validating request fields

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,5 +1,25 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"log"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+	"github.com/olucascdev/crud-users-go/src/configuration/rest_err"
+	"github.com/olucascdev/crud-users-go/src/controller/model/request"
+)
+
+func CreateUser(c *gin.Context) {
+	log.Println("Init CreateUser controller")
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		errRest := rest_err.NewBadRequestError("Some fields are incorrect")
+
+		c.JSON(errRest.Code, errRest)
+		return
+
+	}
+	fmt.Println(userRequest)
+}
